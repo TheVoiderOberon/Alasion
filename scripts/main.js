017d07d1b6f351a585b4c16f50922a9f5dcde2c1
TheVoiderOberon <theangelurdoberoiderreborn45@gmail.com>
2025-01-05 03:11:49 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 const target = "assemblyDrone";
-const baseColor = Color.valueOf("a7b5f7");
+const baseColor1 = Color.valueOf("a7b5f7");
 let palette = [
     Color.valueOf("707cd7"),
     baseColor,
@@ -65,6 +65,17 @@ Events.on(ContentInitEvent, () => {
     Frost.hasPalette = true;
     Frost.emoji = Fonts.getUnicodeStr(target);
 });
+const baseColor2 = Color.valueOf("230f60");
+let palette2 = [
+    Color.valueOf("11072d"),
+    baseColor,
+    Color.valueOf("3f219a")
+];
+let palettei2 = [
+    Color.valueOf("11072d").rgba(),
+    baseColor.rgba(),
+    Color.valueOf("3f219a").rgba()
+];
 Events.on(ContentInitEvent, () => {
 
     let fonts = [Fonts.def, Fonts.outline];
@@ -107,14 +118,14 @@ Events.on(ContentInitEvent, () => {
     Reflect.set(Team, Nebulon, "color", baseColor);
 
     let newPal = Reflect.get(Team, Nebulon, "palette");
-    newPal[0] = palette[0];
-    newPal[1] = palette[1];
-    newPal[2] = palette[2];
+    newPal[0] = palette2[0];
+    newPal[1] = palette2[1];
+    newPal[2] = palette2[2];
     Reflect.set(Team, Nebulon, "palette", newPal);
     let newI = Reflect.get(Team, Nebulon, "palettei");
-    newI[0] = palettei[0];
-    newI[1] = palettei[1];
-    newI[2] = palettei[2];
+    newI[0] = palettei2[0];
+    newI[1] = palettei2[1];
+    newI[2] = palettei2[2];
     Reflect.set(Team, Nebulon, "palettei", newI);
 
     Nebulon.hasPalette = true;
